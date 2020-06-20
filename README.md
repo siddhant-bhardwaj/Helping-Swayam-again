81390eb13a4d351478c8115a65ff684f7445deaa
siddhant-bhardwaj <61611845+siddhant-bhardwaj@users.noreply.github.com>
2020-06-20 14:15:42 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Helping-Swayam-again
+# A-timepass-Java-code
---
+++ student.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+class student{
+String name;
+String name1;
+void main(){
+Scanner obj = new Scanner(System.in);
+System.out.println("Enter your name");
+name1 = obj.next();
+System.out.println("Hello");
+
+
+
+
+}
+
+
+
+
+
+}
